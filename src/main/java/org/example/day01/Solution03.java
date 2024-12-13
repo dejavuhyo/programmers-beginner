86af44c300f8505dd0694dfe724e05444da7fa58
공대베짱이 <dejavuhyo@gmail.com>
2024-12-13 16:04:38 +0900

    feat(solution): create day01 solution

---
+++ src/main/java/org/example/day01/Solution03.java
@@ -0,0 +1,16 @@
+package org.example.day01;
+
+public class Solution03 {
+    /**
+     * 두 수의 곱
+     */
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 4;
+        int answer = 0;
+
+        answer = num1 * num2;
+
+        System.out.println(answer);
+    }
+}
---
+++ src/main/java/org/example/day01/Solution04.java
@@ -0,0 +1,16 @@
+package org.example.day01;
+
+public class Solution04 {
+    /**
+     * 몫 구하기
+     */
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        int answer = 0;
+
+        answer = num1 / num2;
+
+        System.out.println(answer);
+    }
+}
