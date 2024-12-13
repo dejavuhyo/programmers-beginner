99d2c5bf568b0acf9271c01b9b33c22760e684a1
공대베짱이 <dejavuhyo@gmail.com>
2024-12-13 16:05:57 +0900

    feat(solution): create day02 solution

---
+++ src/main/java/org/example/day02/Solution02.java
@@ -0,0 +1,19 @@
+package org.example.day02;
+
+public class Solution02 {
+    /**
+     * 숫자 비교하기
+     */
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 3;
+        int answer = 0;
+
+        if (num1 == num2)
+            answer = 1;
+        else
+            answer = -1;
+
+        System.out.println(answer);
+    }
+}
